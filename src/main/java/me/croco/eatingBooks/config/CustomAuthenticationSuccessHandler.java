bad518d9b1a8a64949f99125f0b74d03cd558822
cr0c0 <hyde69ciel@gmail.com>
2024-03-20 01:16:10 +0900

    #240320 CustomAuthenticationSuccessHandler 수정,  CustomAuthenticationFailureHandler 추가, WebSecurityConfig에 핸들러 적용, loginProcessingUrl 추가

--- src/main/java/me/croco/eatingBooks/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/me/croco/eatingBooks/config/CustomAuthenticationSuccessHandler.java
@@ -12,13 +12,14 @@ import java.io.IOException;
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().print("Login Success");
-        response.getWriter().flush();
+//        response.setContentType("application/json");
+//        response.setCharacterEncoding("UTF-8");
+//        response.getWriter().print("Login Success");
+//        response.getWriter().flush();
     }
 }
