cbc359eb3ea2fa11fb6f76f93e979da97705ce88
cr0c0 <hyde69ciel@gmail.com>
2024-04-07 23:52:33 +0900

    #240407 네이버 책 검색 API 구현 성공

--- src/main/java/me/croco/eatingBooks/naver/dto/NaverBooksListResponse.java
+++ src/main/java/me/croco/eatingBooks/naver/dto/NaverBooksListResponse.java
@@ -16,5 +16,8 @@ import java.util.List;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NaverBooksListResponse {
-    private List<NaverBook> items;
+    private int total;  // 총 검색 결과 개수
+    private int start;  // 검색 시작 위치
+    private int display;    // 한 번에 표시할 검색 결과 개수
+    private List<NaverBook> items;  // 책 검색 결과
 }
--- src/main/java/me/croco/eatingBooks/naver/service/NaverBooksApiService.java
+++ src/main/java/me/croco/eatingBooks/naver/service/NaverBooksApiService.java
@@ -32,14 +32,10 @@ public class NaverBooksApiService {
     private HttpConnection httpConnection;
 
     // 상품 검색 URL
-    // 필수값 : ttbkey, Query(검색어)
     private final String findUrl = "https://openapi.naver.com/v1/search/book.json";
 
     // 상품 조회 URL
-    // 필수값 : ttbkey, ItemId(알라딘 고유값 정수 혹은 ISBN), ItemIdType(ISBN(기본값) : ISBN 10자리, ISBN13 : ISBN 13자리, ItemId)
-    //                                                        조회용 파라미터인 ItemId가 ISBN으로 입력되었는지, 알라딘고유의"ItemId"값으로 입력되었는지 선택
-    //                                                        가급적 13자리 ISBN을 이용해주세요
-    private final String findByIdUrl = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx";
+    private final String findByIdUrl = "https://openapi.naver.com/v1/search/book_adv.xml";
 
 
 
@@ -50,7 +46,7 @@ public class NaverBooksApiService {
 //        keyword = keyword.contains(" ") ? keyword.replace(" ", "+") : keyword;  // 검색어에 공백 존재시 '+'로 바꿔서 검색
 
         int start = request.getStart();
-        start = start == 0 ? 1 : start;
+        start = start == 0 ? 1 : (start-1) * 10 + 1;
 
         String apiUrl = UriComponentsBuilder.fromUriString(findUrl)
                 .queryParam("query", request.getKeyword())
@@ -77,38 +73,36 @@ public class NaverBooksApiService {
         }
         return response;
     }
-//
-//    public NaverBookResponse findBook(String isbn) {
-//        String apiUrl = UriComponentsBuilder.fromUriString(findByIdUrl)
-//                .queryParam("ttbkey", ttbKey)
-//                .queryParam("ItemId", isbn)
-//                .queryParam("ItemIdType", "ISBN13")
-//                .queryParam("Cover", "Big")
-//                .queryParam("Output", "js")
-//                .queryParam("Version", "20131101")
-//                .build()
-//                .encode()
-//                .toUriString();
-//
-//        HttpURLConnection httpURLConnection = getHttpURLConnection(apiUrl);
-//
-//        String result = getHttpResponse(httpURLConnection);
-//
-//        ObjectMapper mapper = JsonMapper.builder()
-//                .enable(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER)
-//                .build();
-//
-//        NaverBookResponse response = null;
-//
-//        try {
-//            response = mapper.readValue(result, NaverBookResponse.class);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return response;
-//
-//
-//
-//    }
+
+    public NaverBookResponse findBook(String isbn) {
+        String apiUrl = UriComponentsBuilder.fromUriString(findByIdUrl)
+                .queryParam("d_isbn", isbn)
+                .queryParam("display", 1)
+                .queryParam("start", "1")
+                .build()
+                .encode()
+                .toUriString();
+
+        HttpURLConnection httpURLConnection = getHttpURLConnection(apiUrl);
+        httpURLConnection.setRequestProperty("X-Naver-Client-Id", naverProperties.getClientId());
+        httpURLConnection.setRequestProperty("X-Naver-Client-Secret", naverProperties.getClientSecret());
+        String result = getHttpResponse(httpURLConnection);
+
+        ObjectMapper mapper = JsonMapper.builder()
+                .enable(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER)
+                .build();
+
+        NaverBookResponse response = null;
+
+        try {
+            response = mapper.readValue(result, NaverBookResponse.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+
+
+
+    }
 
 }
