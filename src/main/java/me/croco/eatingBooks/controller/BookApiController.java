1555046c7c6e2242eb624fe03042ba249fe76d23
cr0c0 <hyde69ciel@gmail.com>
2024-04-15 22:36:23 +0900

    #240415 식전문, 식후문 많은 책 목록 조회 API 추가

--- src/main/java/me/croco/eatingBooks/controller/BookApiController.java
+++ src/main/java/me/croco/eatingBooks/controller/BookApiController.java
@@ -3,27 +3,49 @@ package me.croco.eatingBooks.controller;
 import lombok.RequiredArgsConstructor;
 import me.croco.eatingBooks.api.aladin.service.AladinApiService;
 import me.croco.eatingBooks.api.naver.service.NaverBooksApiService;
+import me.croco.eatingBooks.domain.Book;
 import me.croco.eatingBooks.dto.BookFindRequest;
 import me.croco.eatingBooks.dto.BookFindResponse;
+import me.croco.eatingBooks.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BookApiController {
 
-    private final AladinApiService bookApiService;
+    private final BookService bookApiService;
     //private final NaverBooksApiService bookApiService;
 
     @PostMapping("/api/books")
     public ResponseEntity<BookFindResponse> searchBooks(@RequestBody BookFindRequest request) {
         return ResponseEntity.ok()
-                .body(new BookFindResponse(bookApiService.searchBooks(request)));
+                .body(bookApiService.searchBooks(request));
     }
 
     @GetMapping("/api/books/{isbn}")
     public ResponseEntity<BookFindResponse> findBook(@PathVariable String isbn) {
         return ResponseEntity.ok()
-                .body(new BookFindResponse(bookApiService.findBook(isbn)));
+                .body(bookApiService.findBook(isbn));
+    }
+
+    @GetMapping("/api/books/best")
+    public ResponseEntity<BookFindResponse> findBestSellerList() {
+        return ResponseEntity.ok()
+                .body(bookApiService.findBestSellerList());
+    }
+
+    @GetMapping("/api/books/best/before")
+    public ResponseEntity<List<Book>> findBestBeforeArticle() {
+        return ResponseEntity.ok()
+                .body(bookApiService.findBestBeforeArticle());
+    }
+
+    @GetMapping("/api/books/best/after")
+    public ResponseEntity<List<Book>> findBestAfterArticle() {
+        return ResponseEntity.ok()
+                .body(bookApiService.findBestAfterArticle());
     }
 }
--- src/main/java/me/croco/eatingBooks/repository/ArticleRepository.java
+++ src/main/java/me/croco/eatingBooks/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package me.croco.eatingBooks.repository;
 
 import me.croco.eatingBooks.domain.Article;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +10,10 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     @Query("SELECT a FROM Article a WHERE a.publicYn = 'true' ORDER BY a.createdAt DESC ")
     List<Article> findPublicArticles();
+
+    @Query("SELECT a.isbn, COUNT(a.id) as count FROM Article a WHERE a.articleType = 'B' GROUP BY a.isbn ORDER BY count DESC")
+    List<Object[]> findBestIsbnBeforeArticle(Pageable pageable);
+
+    @Query("SELECT a.isbn, COUNT(a.id) as count FROM Article a WHERE a.articleType = 'A' GROUP BY a.isbn ORDER BY count DESC")
+    List<Object[]> findBestIsbnAfterArticle(Pageable pageable);
 }
